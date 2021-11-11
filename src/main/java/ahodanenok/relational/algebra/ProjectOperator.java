6e54b1d25c69b4eafb8dc2724f1e593ddb5d04ee
Andrei Hodanenok <xoda91@gmail.com>
2021-11-11 22:18:34 +0300

    Add 'project' operator.

---
+++ src/main/java/ahodanenok/relational/algebra/ProjectOperator.java
@@ -0,0 +1,89 @@
+package ahodanenok.relational.algebra;
+
+import ahodanenok.relational.*;
+import ahodanenok.relational.exception.AttributeNotFoundException;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+/**
+ * Projection of a single tuple.
+ * <p>
+ * Produces a new relation with a subset of its attributes.
+ * Depending on the mode specified attributes are excluded or the ones being left.
+ * Default mode is to leave specified attributes.
+ */
+public final class ProjectOperator implements RelationalOperator {
+
+    private final Relation relation;
+    private final Set<String> attributeNames;
+    private boolean included = true;
+
+    public ProjectOperator(Relation relation, String... attributeNames) {
+        this(relation, Arrays.asList(attributeNames));
+    }
+
+    public ProjectOperator(Relation relation, List<String> attributeNames) {
+        Objects.requireNonNull(relation, "relation can't be null");
+        Objects.requireNonNull(attributeNames, "attribute names can't be null");
+        this.relation = relation;
+        this.attributeNames = new HashSet<>(attributeNames);
+    }
+
+    /**
+     * Produce a new relation only with specified attributes.
+     * This is default mode.
+     */
+    public ProjectOperator includeAttributes() {
+        this.included = true;
+        return this;
+    }
+
+    /**
+     * Produce a new relation without specified attributes
+     */
+    public ProjectOperator withoutAttributes() {
+        this.included = false;
+        return this;
+    }
+
+    @Override
+    public Relation execute() {
+        if (attributeNames.isEmpty() && included) {
+            return Relation.EMPTY;
+        }
+
+        RelationSchema schema = relation.schema();
+        for (String name : attributeNames) {
+            if (!schema.hasAttribute(name)) {
+                throw new AttributeNotFoundException(name);
+            }
+        }
+
+        RelationSchema resultSchema = projectSchema();
+        RelationSelector resultRelationSelector = new RelationSelector().withSchema(resultSchema);
+        relation.tuples().map(t -> projectTuple(t, resultSchema)).forEach(resultRelationSelector::addTuple);
+
+        return resultRelationSelector.select();
+    }
+
+    private RelationSchema projectSchema() {
+        RelationSchemaGenerator resultSchemaGenerator = new RelationSchemaGenerator();
+        Predicate<Attribute> predicate = a -> attributeNames.contains(a.getName());
+        if (!included) {
+            predicate = predicate.negate();
+        }
+        relation.schema().getAttributes().stream().filter(predicate).forEach(resultSchemaGenerator::withAttribute);
+
+        return resultSchemaGenerator.generate();
+    }
+
+    private Tuple projectTuple(Tuple tuple, RelationSchema resultSchema) {
+        TupleSelector tupleSelector = new TupleSelector();
+        for (Attribute a : resultSchema.getAttributes()) {
+            tupleSelector.withValue(a.getName(), tuple.getValue(a.getName()));
+        }
+
+        return tupleSelector.select();
+    }
+}
---
+++ src/test/java/ahodanenok/relational/ProjectOperatorTest.java
@@ -0,0 +1,133 @@
+package ahodanenok.relational;
+
+import ahodanenok.relational.algebra.ProjectOperator;
+import ahodanenok.relational.exception.AttributeNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProjectOperatorTest {
+
+    @Test
+    public void shouldProject0Relation() {
+        assertEquals(Relation.EMPTY, new ProjectOperator(Relation.EMPTY).execute());
+        assertEquals(Relation.EMPTY, new ProjectOperator(Relation.EMPTY).withoutAttributes().execute());
+    }
+
+    @Test
+    public void shouldProjectEmptyRelation() {
+        RelationSchema schema = new RelationSchemaGenerator()
+                .withAttribute("a", Integer.class)
+                .withAttribute("b", Boolean.class)
+                .withAttribute("c", String.class)
+                .generate();
+
+        Relation relation = new RelationSelector().withSchema(schema).select();
+
+        RelationSchema expectedIncluded = new RelationSchemaGenerator()
+                .withAttribute("b", Boolean.class)
+                .generate();
+        assertEquals(
+                new RelationSelector().withSchema(expectedIncluded).select(),
+                new ProjectOperator(relation, "b").execute());
+
+        RelationSchema expectedExcluded = new RelationSchemaGenerator()
+                .withAttribute("a", Integer.class)
+                .withAttribute("c", String.class)
+                .generate();
+        assertEquals(
+                new RelationSelector().withSchema(expectedExcluded).select(),
+                new ProjectOperator(relation, "b").withoutAttributes().execute());
+    }
+
+    @Test
+    public void shouldProjectRelationWithOneTuple() {
+        Relation relation = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").withValue("b", "11").withValue("c", "111").withValue("d", "1111").select())
+                .select();
+
+        Relation expectedIncluded = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").withValue("d", "1111").select())
+                .select();
+        assertEquals(expectedIncluded, new ProjectOperator(relation, "a", "d").execute());
+
+        Relation expectedExcluded = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("b", "11").withValue("c", "111").select())
+                .select();
+        assertEquals(expectedExcluded, new ProjectOperator(relation, "a", "d").withoutAttributes().execute());
+    }
+
+    @Test
+    public void shouldProjectRelationWithMultipleTuples() {
+        Relation relation = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").withValue("b", "11").withValue("c", "111").withValue("d", "1111").select())
+                .addTuple(new TupleSelector().withValue("a", "2").withValue("b", "22").withValue("c", "222").withValue("d", "2222").select())
+                .addTuple(new TupleSelector().withValue("a", "3").withValue("b", "33").withValue("c", "333").withValue("d", "3333").select())
+                .addTuple(new TupleSelector().withValue("a", "4").withValue("b", "44").withValue("c", "444").withValue("d", "4444").select())
+                .select();
+
+        Relation expectedIncluded = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").withValue("c", "111").withValue("d", "1111").select())
+                .addTuple(new TupleSelector().withValue("a", "2").withValue("c", "222").withValue("d", "2222").select())
+                .addTuple(new TupleSelector().withValue("a", "3").withValue("c", "333").withValue("d", "3333").select())
+                .addTuple(new TupleSelector().withValue("a", "4").withValue("c", "444").withValue("d", "4444").select())
+                .select();
+        assertEquals(expectedIncluded, new ProjectOperator(relation, "a", "c", "d").execute());
+
+        Relation expectedExcluded = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("b", "11").select())
+                .addTuple(new TupleSelector().withValue("b", "22").select())
+                .addTuple(new TupleSelector().withValue("b", "33").select())
+                .addTuple(new TupleSelector().withValue("b", "44").select())
+                .select();
+        assertEquals(expectedExcluded, new ProjectOperator(relation, "a", "c", "d").withoutAttributes().execute());
+
+        Relation expectedExcludedAll = new RelationSelector().withSchema(RelationSchema.EMPTY).addTuple(Tuple.EMPTY).select();
+        assertEquals(expectedExcludedAll, new ProjectOperator(relation, "a", "b", "c", "d").withoutAttributes().execute());
+    }
+
+    @Test
+    public void shouldSwitchBetweenModes() {
+        Relation relation = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").withValue("b", "11").withValue("c", "111").select())
+                .select();
+
+        Relation expectedIncluded = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").select())
+                .select();
+
+        Relation expectedExcluded = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("b", "11").withValue("c", "111").select())
+                .select();
+
+        ProjectOperator op = new ProjectOperator(relation, "a");
+        assertEquals(expectedIncluded, op.execute());
+        assertEquals(expectedExcluded, op.withoutAttributes().execute());
+        assertEquals(expectedIncluded, op.includeAttributes().execute());
+        assertEquals(expectedExcluded, op.withoutAttributes().execute());
+        assertEquals(expectedIncluded, op.includeAttributes().execute());
+    }
+
+    @Test
+    public void shouldThrowErrorIfAttributeNotFound() {
+        Relation relation = new RelationSelector()
+                .addTuple(new TupleSelector().withValue("a", "1").withValue("b", "11").select())
+                .select();
+
+        ProjectOperator op = new ProjectOperator(relation, "b", "c");
+        AttributeNotFoundException e = assertThrows(AttributeNotFoundException.class, op::execute);
+        assertEquals("c", e.getName());
+    }
+
+    @Test
+    public void shouldThrowErrorIfNullParameters() {
+        NullPointerException e1 = assertThrows(NullPointerException.class, () -> new ProjectOperator(null, Collections.emptyList()));
+        assertEquals("relation can't be null", e1.getMessage());
+
+        NullPointerException e2 = assertThrows(NullPointerException.class, () -> new ProjectOperator(Relation.EMPTY, (List<String>) null));
+        assertEquals("attribute names can't be null", e2.getMessage());
+    }
+}
